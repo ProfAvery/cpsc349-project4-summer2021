766a55cd6552f1e01be8bef53ae85c4872ca1d7a
Kenytt Avery <kavery@fullerton.edu>
2021-08-01 16:11:50 -0700

    Switch from promises to async/await

--- docs/scripts/init.js
+++ docs/scripts/init.js
@@ -4,18 +4,13 @@ const keyword = document.querySelector('#keyword')
 const resultDiv = document.querySelector('#results')
 const result = document.querySelector('#result-value')
 
-function search (term = '') {
+async function search (term = '') {
   const query = encodeURIComponent(`%%${term}%%`)
-  fetch(`http://localhost:5000/posts/?text=${query}`)
-    .then(response => response.json())
-    .then(data => {
-      if (!term) {
-        resultDiv.hidden = true
-      } else {
-        resultDiv.hidden = false
-        result.textContent = JSON.stringify(data.resources, null, 2)
-      }
-    })
+  const response = await fetch(`http://localhost:5000/posts/?text=${query}`)
+  const data = await response.json()
+
+  result.textContent = JSON.stringify(data.resources, null, 2)
+  resultDiv.hidden = !term
 }
 
 searchForm.addEventListener('submit', (event) => {
